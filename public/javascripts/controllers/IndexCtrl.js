c72b47c9b47347854bfd18c27d2f65a15ca0ee26
Adam-Crowe <jamymonster12@gmail.com>
2016-02-07 21:02:06 +0000

    Created Home Page

--- public/javascripts/controllers/IndexCtrl.js
+++ public/javascripts/controllers/IndexCtrl.js
@@ -1,7 +1,24 @@
 angular.module("grubsup.controllers").
   controller("IndexCtrl", [
     "$scope",
-    function ($scope) {
-      $scope.title = "Grub's Up!";
+    "$location",
+    "$window",
+    function ($scope, $location, $window) {
+      $("#login-signup-tabs .nav-item .nav-link").on("mouseup", function () {
+        if($(this).hasClass("signup-tab")){
+          $(".signup-tab").addClass("active");
+          $(".login-tab").removeClass("active");
+        }
+        else if($(this).hasClass("login-tab")){
+          $(".login-tab").addClass("active");
+          $(".signup-tab").removeClass("active");
+        }
+      });
+
+      $(".top").on("mouseup", function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600)
+      });
     }
   ]);
